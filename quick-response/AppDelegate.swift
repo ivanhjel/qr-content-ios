62675542145bbdae24fe58df702a9c81ca4ed76b
Ivan Hjelmeland <ivan.hjelmeland@gmail.com>
2022-08-15 08:48:32 +0200

    init Xcode project.

---
+++ quick-response/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  quick-response
+//
+//  Created by Ivan Hjelmeland on 15/08/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
